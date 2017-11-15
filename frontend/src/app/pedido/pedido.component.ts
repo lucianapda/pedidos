23caf0568955bc0c92291f3faa48d960cb0a5cd0
BrunoBkuzr <bbf1234@gmail.com>
2017-11-15 12:36:53 -0200

    Criado tela de cadastro de pedidos, faltam ajustes

--- frontend/src/app/pedido/pedido.component.ts
+++ frontend/src/app/pedido/pedido.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import {Produto} from '../produto/Produto'
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import { Component, OnInit, ViewEncapsulation, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-pedido',
   templateUrl: './pedido.component.html',
   styleUrls: ['./pedido.component.css'],
   encapsulation: ViewEncapsulation.None
+  
 })
 export class PedidoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
